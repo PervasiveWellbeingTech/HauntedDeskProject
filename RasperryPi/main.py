3ad28b7789b2b6588924c5722cb95fe4d942f7ce
hugo-alayrangues <hugo.alayrangues@orange.fr>
2019-11-19 19:46:11 +0000

    Code cleaning.

--- RasperryPi/main.py
+++ RasperryPi/main.py
@@ -33,8 +33,7 @@ radio.enableAckPayload()
 
 START_RECORD = 20
 STOP_RECORD = 50
-recorded = False
-record = None
+record = None  
 counter = 0
 
 recorded_desk = [0] * len(desks)
@@ -62,7 +61,7 @@ while True:
                     if LOG:
                         record.write_log("INFO: Desk " + desks[i])
                         
-                    print("Pipe", i, "- counter:", counter)
+                    print("Pipe", i, "- day:", counter)
                     radio.openWritingPipe(pipe[0])
                     radio.openReadingPipe(1, pipe[1])
                     
@@ -85,7 +84,7 @@ while True:
                             break
                         recorded_desk[i] = 1
                         if LOG:
-                            record.write_log("INFO: Pipe " + str(i) + " , success record.")
+                            record.write_log("INFO: Desk " + desks[i] + " , success record.")
                     else:
                         warning_message = file_info[0]
                         if LOG:
@@ -93,126 +92,10 @@ while True:
                     print()
                 else:
                     if LOG:
-                        record.write_log("INFO: Pipe " + str(i) + " already recorded.")
+                        record.write_log("INFO: Desk " + desks[i] + " already recorded.")
                         
         if LOG:
             record.write_log("INFO: End of recording")                
         recorded_desk = [0] * len(desks)           
-##    elif current_time.second < LIMIT:
-##        print("Wait", current_time.second)
-##        recorded = False
     else:
         print("Wait", current_time.second)
-
-
-"""
-LIMIT = 30
-recorded = False
-record = None
-counter = 0
-
-recorded_desk = [0] * len(desks)
-
-while True:
-    current_time = datetime.now()
-    time.sleep(3)
-    
-    if current_time.second >= LIMIT and not recorded:
-        recorded = True
-        print()
-        
-        if sum(recorded_desk) == 0:
-            counter += 1
-            record = Record(DATA_PATH + str(counter) + "_")
-        
-        if LOG:
-            record.write_log("Record created")
-        
-        for i, pipe in enumerate(pipes):
-            if not recorded_desk[i]:
-                if LOG:
-                    record.write_log("Pipe " + str(i))
-                    
-                print("Pipe", i, "- counter:", counter)
-                radio.openWritingPipe(pipe[0])
-                radio.openReadingPipe(1, pipe[1])
-                
-                utils.send_message(radio, "data", DEBUG)
-
-                file_info_received, file_info = utils.listen_file_info(radio, DEBUG)
-                
-                if file_info_received:
-                    file_name, file_size = file_info
-                    
-                    if LOG:
-                        record.write_log(file_name)
-                        record.write_log(file_size + " bytes")
-                    
-                    for _ in range(100):
-                        utils.send_message(radio, "ok", DEBUG)
-                    utils.receive_file(radio, DATA_PATH + str(i) + "_" + file_name, file_size, DEBUG)
-                    recorded_desk[i] = 1
-                    if LOG:
-                        record.write_log("Pipe " + str(i) + " , success record.")
-                else:
-                    if LOG:
-                        record.write_log("WARNING: File info not received")
-                
-                if LOG:
-                    record.write_log("End of recording")
-                print()
-            else:
-                if LOG:
-                    record.write_log("Pipe " + str(i) + " already recorded.")
-    elif current_time.second < LIMIT:
-        print("Wait", current_time.second)
-        recorded = False
-    else:
-        print("Wait", current_time.second)
-"""
-
-# ------------ END OF WIP
-
-"""
-
-counter = 0
-
-#while True:
-for _ in range(1):
-    print()
-    counter += 1
-    record = Record(DATA_PATH)
-    
-    if LOG:
-        record.write_log("Record created")
-    
-    for i, pipe in enumerate(pipes):
-        if LOG:
-            record.write_log("Pipe " + str(i))
-            
-        print("Pipe", i, "- counter:", counter)
-        radio.openWritingPipe(pipe[0])
-        radio.openReadingPipe(1, pipe[1])
-        
-        utils.send_message(radio, "data", DEBUG)
-
-        file_info_received, file_info = utils.listen_file_info(radio, DEBUG)
-        
-        if file_info_received:
-            file_name, file_size = file_info
-            
-            if LOG:
-                record.write_log(file_name)
-                record.write_log(file_size + " bytes")
-            
-            for _ in range(100):
-                utils.send_message(radio, "ok", DEBUG)
-            utils.receive_file(radio, DATA_PATH + str(i) + "_" + file_name, file_size, DEBUG)
-        else:
-            if LOG:
-                record.write_log("WARNING: File info not received")
-        
-        if LOG:
-            record.write_log("End of recording")
-        print()
-"""
