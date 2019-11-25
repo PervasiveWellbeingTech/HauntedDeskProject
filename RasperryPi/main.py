2fce80ff2d29dcbcc493570d442d9b3bd2048fa7
hugo-alayrangues <hugo.alayrangues@orange.fr>
2019-11-25 22:56:23 +0000

    Main process split into several functions, better handling of time intervals change.

--- RasperryPi/main.py
+++ RasperryPi/main.py
@@ -8,6 +8,7 @@ from record import Record
 
 DEBUG = True
 DATA_PATH = "data/"
+OK_RETRIES = 100
 
 GPIO.setmode(GPIO.BCM)                 # set the gpio mode
 
@@ -28,80 +29,88 @@ radio.setRetries(0, 125)
 radio.enableAckPayload()
 
 
-START_RECORD = dt.timedelta(hours=21).seconds  # 21 hours in seconds
-STOP_RECORD = dt.timedelta(hours=6).seconds    # 6 hours in seconds
+START_RECORD = dt.timedelta(hours=12).seconds
+STOP_RECORD = dt.timedelta(hours=14, minutes=41).seconds
 DAY_IN_SECONDS = 24 * 3600
-record = None
 
-recorded_desk = [0] * len(desks)
+record = None
+recorded_desks = [0] * len(desks)
+
+
+def is_time_in_interval(current_time, start_time, stop_time):
+    if start_time <= stop_time:
+        return start_time <= current_time < stop_time
+    else:
+        return start_time <= current_time or current_time < stop_time
+
+
+def is_new_day(recorded_desks):
+    return sum(recorded_desks) == 0
+    
+
+def are_all_desks_recorded(recorded_desks):
+    return sum(recorded_desks) == len(recorded_desks)
+
+
+def record_desk(radio, record, desk_name, recorded_desks):
+    record.write_log("INFO: Desk " + desk_name)
+                            
+    print("Pipe", i)
+    radio.openWritingPipe(pipe[0])
+    radio.openReadingPipe(1, pipe[1])
+    
+    utils.send_message(radio, "data", DEBUG)
+    file_info_received, file_info = utils.listen_file_info(radio, DEBUG)
+    
+    if file_info_received:
+        file_name, file_size = file_info
+        record.write_log("INFO: File info received ({} - {} bytes)".format(file_name, file_size))
+        
+        for _ in range(OK_RETRIES):
+            utils.send_message(radio, "ok", DEBUG)
+        
+        file_reception = utils.real_timed_receive_file(radio, record.path + "/" + desk_name + "_" + file_name,
+                                                       file_size, STOP_RECORD, START_RECORD, DEBUG)
+        if file_reception == "interrupted":
+            record.write_log("WARNING: Transmission interrupted.")
+            return "interrupted"
+        recorded_desks[i] = 1
+        record.write_log("INFO: Desk " + desk_name + " , success record.")
+    else:
+        warning_message = file_info[0]
+        record.write_log("WARNING: " + warning_message)
+    print()
+
 
 while True:
-    current_time = dt.datetime.now()
     time.sleep(3)
-    
     current_time_in_seconds = utils.get_time_in_seconds()
     
-    if current_time_in_seconds >= START_RECORD or current_time_in_seconds < STOP_RECORD:
+    if is_time_in_interval(current_time_in_seconds, START_RECORD, STOP_RECORD):
         
-        if sum(recorded_desk) == 0:
-            print("New day")
+        if is_new_day(recorded_desks):
             record = Record(DATA_PATH)
+            record.write_log("INFO: Record created")
         
-        record.write_log("INFO: Record created")
-        
-        while current_time_in_seconds >= START_RECORD or current_time_in_seconds < STOP_RECORD:
-            print(current_time_in_seconds >= START_RECORD or current_time_in_seconds < STOP_RECORD)
-            print(current_time_in_seconds)
+        while is_time_in_interval(current_time_in_seconds, START_RECORD, STOP_RECORD):
+            print("Transmission", current_time_in_seconds)
             current_time_in_seconds = utils.get_time_in_seconds()
             
-            if sum(recorded_desk) != len(recorded_desk):
-                print("In while")
+            if not are_all_desks_recorded(recorded_desks):
                 for i, pipe in enumerate(pipes):
-                    if not recorded_desk[i]:
-                        record.write_log("INFO: Desk " + desks[i])
-                            
-                        print("Pipe", i)
-                        radio.openWritingPipe(pipe[0])
-                        radio.openReadingPipe(1, pipe[1])
-                        
-                        utils.send_message(radio, "data", DEBUG)
-
-                        file_info_received, file_info = utils.listen_file_info(radio, DEBUG)
-                        
-                        if file_info_received:
-                            file_name, file_size = file_info
-                            
-                            record.write_log("INFO: File info received ({} - {} bytes)".format(file_name, file_size))
-                            
-                            for _ in range(100):
-                                utils.send_message(radio, "ok", DEBUG)
-                                  
-                            temp_time_in_seconds = utils.get_time_in_seconds()
-                            
-                            temp_stop_record = 0
-                            if temp_time_in_seconds > START_RECORD:
-                                temp_stop_record = DAY_IN_SECONDS - temp_time_in_seconds + STOP_RECORD;
-                            elif temp_time_in_seconds < STOP_RECORD:
-                                temp_stop_record = STOP_RECORD - temp_time_in_seconds
-                            
-                            file_reception = utils.real_timed_receive_file(radio, record.path + "/" + desks[i] + "_" + file_name,
-                                                                           file_size, STOP_RECORD, START_RECORD, DEBUG)
-                            if file_reception == "interrupted":
-                                record.write_log("WARNING: Transmission interrupted.")
-                                break
-                            recorded_desk[i] = 1
-                            record.write_log("INFO: Desk " + desks[i] + " , success record.")
-                        else:
-                            warning_message = file_info[0]
-                            record.write_log("WARNING: " + warning_message)
-                        print()
+                    if not recorded_desks[i]:
+                        desk_name = desks[i]
+                        record_result = record_desk(radio, record, desk_name, recorded_desks)
+                        if record_result and record_result == "interrupted":
+                            break
                     else:
-                        record.write_log("INFO: Desk " + desks[i] + " already recorded.")
+                        record.write_log("INFO: Desk " + desk_name + " already recorded.")
                             
         record.write_log("INFO: End of recording")                
-        recorded_desk = [0] * len(desks)           
+        recorded_desks = [0] * len(desks)           
     else:
         print("Wait", current_time_in_seconds)
+
 
 # ------------ 1 minute day simulation (30s day, 30s night)
 """
@@ -110,7 +119,7 @@ STOP_RECORD = 50
 record = None  
 counter = 0
 
-recorded_desk = [0] * len(desks)
+recorded_desks = [0] * len(desks)
 
 while True:
     current_time = dt.datetime.now()
@@ -120,7 +129,7 @@ while True:
         
         print("Go", current_time.second)
         
-        if sum(recorded_desk) == 0:
+        if sum(recorded_desks) == 0:
             print("New day")
             counter += 1  # simulates a new day
             record = Record(DATA_PATH + str(counter) + "_")
@@ -128,10 +137,10 @@ while True:
         record.write_log("INFO: Record created")
         
         while dt.datetime.now().second < STOP_RECORD:
-            if sum(recorded_desk) != len(recorded_desk):
+            if sum(recorded_desks) != len(recorded_desks):
                 print("In while")
                 for i, pipe in enumerate(pipes):
-                    if not recorded_desk[i]:
+                    if not recorded_desks[i]:
                         record.write_log("INFO: Desk " + desks[i])
                             
                         print("Pipe", i, "- day:", counter)
@@ -153,7 +162,7 @@ while True:
                             if file_reception == "interrupted":
                                 record.write_log("WARNING: Transmission interrupted.")
                                 break
-                            recorded_desk[i] = 1
+                            recorded_desks[i] = 1
                             record.write_log("INFO: Desk " + desks[i] + " , success record.")
                         else:
                             warning_message = file_info[0]
@@ -163,7 +172,7 @@ while True:
                         record.write_log("INFO: Desk " + desks[i] + " already recorded.")
                             
         record.write_log("INFO: End of recording")                
-        recorded_desk = [0] * len(desks)           
+        recorded_desks = [0] * len(desks)           
     else:
         print("Wait", current_time.second)
 """
